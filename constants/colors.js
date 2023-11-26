bb50300ebb7f13939c2f00d9242403971e24b654
Mohit Shende <mohit@tartanhq.com>
2023-11-26 21:05:39 +0530

    added color constants

---
+++ constants/colors.js
@@ -0,0 +1,5 @@
+export const PRIMARY_COLOR = "#001A72";
+export const ACCENT_COLOR = "#782AEB";
+export const BACKGROUND_COLOR = "#F8F9FF";
+export const BORDER_COLOR = "#C1C6E5";
+export const BACKDROP_COLOR = "rgba(0, 0, 0, 0.3)";
